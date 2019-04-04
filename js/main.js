eb451ef0b952cbae7daac4c8915cdac811150bec
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2019-04-04 15:14:10 -0400

    added a second dataset

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
   let lineChart = new Chart(CHART, {
     type: 'line',
     data: {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      labels: ["January", "February", "March", "April", "May", "June", "July", "August"],
       datasets: [
         {
           label: "My First Dataset",
@@ -25,8 +25,29 @@
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
-          data: [65, 59, 80, 81, 56, 55, 40],
-        }
+          data: [65, 59, 80, 81, 56, 55, 40, 50],
+        },
+        {
+          label: "My Second Dataset",
+          fill: true,
+          lineTension: 0.1,
+          backgroundColor: "rgba(75, 75, 192, 0.4)",
+          borderColor: "rgba(75, 72, 192, 1",
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: "rgba(75, 72, 192, 1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(75, 72, 192, 1)",
+          pointHoverBorderColor: "rgba(220, 220, 220, 1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [100, 20, 60, 20, 80, 55, 90],
+        },
       ]
     }
   });
