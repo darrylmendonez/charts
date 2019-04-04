a29bd0df7b44d139f158d4d6165a3eedf388e7f9
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2019-04-04 15:52:20 -0400

    created a bar chart that beginsAtZero

--- js/main.js
+++ js/main.js
@@ -1,64 +1,20 @@
 const CHART = document.getElementById("line-chart");
 
-Chart.defaults.global.animation.duration = 500;
+Chart.defaults.scale.ticks.beginAtZero = true;
 
-let lineChart = new Chart(CHART, {
-  type: 'line',
+let barChart = new Chart(CHART, {
+  type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June", "July", "August"],
+    labels: ['Jan', 'Feb', 'March', 'April'],
     datasets: [
       {
-        label: "My First Dataset",
-        fill: false,
-        lineTension: 0.1,
-        backgroundColor: "rgba(75, 192, 192, 0.4)",
-        borderColor: "rgba(75, 192, 192, 1",
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: "rgba(75, 192, 192, 1)",
-        pointBackgroundColor: "#fff",
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: "rgba(75, 192, 192, 1)",
-        pointHoverBorderColor: "rgba(220, 220, 220, 1)",
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: [65, 59, 80, 81, 56, 55, 40, 50],
-      },
-      {
-        label: "My Second Dataset",
-        fill: true,
-        lineTension: 0.5,
-        backgroundColor: "rgba(75, 75, 192, 0.4)",
-        borderColor: "rgba(75, 72, 192, 1",
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: "rgba(75, 72, 192, 1)",
-        pointBackgroundColor: "#fff",
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: "rgba(75, 72, 192, 1)",
-        pointHoverBorderColor: "rgba(220, 220, 220, 1)",
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: [100, 20, 60, 20, 80, 55, 90],
-      },
+        label: 'Number Per Month',
+        backgroundColor: '#00FF00',
+        borderColor: '#00FF00',
+        borderWidth: 2,
+        data: [10, 20, 55, 30],
+      }
     ]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true,
-        }
-      }]
-    }
   }
 });
 
