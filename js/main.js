e624056b3101e64503d00ab46f36a9d914309a3d
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2019-04-04 18:01:29 -0400

    created a logarithmic line graph

--- js/main.js
+++ js/main.js
@@ -3,22 +3,26 @@ const CHART = document.getElementById("line-chart");
 Chart.defaults.scale.ticks.beginAtZero = true;
 
 let barChart = new Chart(CHART, {
-  type: 'doughnut',
+  type: 'line',
   data: {
     labels: ['Strength', 'Skill', 'Health', 'Speed', 'Luck'],
     datasets: [
       {
         label: 'Points',
-        backgroundColor: ['#f1c40f', '#e67e22', '#16a085', '#2980b9', '#8e44ad'],
+        borderColor: '#2980b9',
         data: [10, 20, 55, 30, 10],
       },
     ]
   },
   options: {
-    cutoutPercentage: 20,
-    rotation: Math.PI * 0.5,
-    animation: {
-      animateScale: true
+    scales: {
+      yAxes: [{
+        type: 'logarithmic',
+        ticks: {
+          min: 1,
+          max: 60
+        }
+      }]
     }
   }
 });
