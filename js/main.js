435ebcdf601d3c39d37432dc34caf25137d806a5
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2019-04-04 16:10:13 -0400

    created polarArea chart

--- js/main.js
+++ js/main.js
@@ -3,25 +3,29 @@ const CHART = document.getElementById("line-chart");
 Chart.defaults.scale.ticks.beginAtZero = true;
 
 let barChart = new Chart(CHART, {
-  type: 'radar',
+  type: 'polarArea',
   data: {
     labels: ['Strength', 'Skill', 'Health', 'Speed', 'Luck'],
     datasets: [
       {
         label: 'Points',
-        backgroundColor: 'rgba(00, 255, 00, 0.1)',
-        borderColor: '#00FF00',
+        backgroundColor: ['#f1c40f', '#e67e22', '#16a085', '#2980b9', '#8e44ad'],
         borderWidth: 2,
         data: [10, 20, 55, 30, 10],
       },
-      {
-        label: 'Points',
-        backgroundColor: 'rgba(0, 255, 255, 0.1)',
-        borderColor: '#00FFFF',
-        borderWidth: 2,
-        data: [5, 100, 20, 70, 80],
-      }
+      // {
+      //   label: 'Points',
+      //   backgroundColor: 'rgba(0, 255, 255, 0.1)',
+      //   borderColor: '#00FFFF',
+      //   borderWidth: 2,
+      //   data: [5, 100, 20, 70, 80],
+      // }
     ]
+  },
+  options: {
+    animation: {
+      animateScale: false
+    }
   }
 });
 
