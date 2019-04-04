b955cf9246eecb72c0ebb92e29eaac9d3af4cc24
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2019-04-04 17:48:25 -0400

    created pie chart

--- js/main.js
+++ js/main.js
@@ -3,28 +3,20 @@ const CHART = document.getElementById("line-chart");
 Chart.defaults.scale.ticks.beginAtZero = true;
 
 let barChart = new Chart(CHART, {
-  type: 'polarArea',
+  type: 'pie',
   data: {
     labels: ['Strength', 'Skill', 'Health', 'Speed', 'Luck'],
     datasets: [
       {
         label: 'Points',
         backgroundColor: ['#f1c40f', '#e67e22', '#16a085', '#2980b9', '#8e44ad'],
-        borderWidth: 2,
         data: [10, 20, 55, 30, 10],
       },
-      // {
-      //   label: 'Points',
-      //   backgroundColor: 'rgba(0, 255, 255, 0.1)',
-      //   borderColor: '#00FFFF',
-      //   borderWidth: 2,
-      //   data: [5, 100, 20, 70, 80],
-      // }
     ]
   },
   options: {
     animation: {
-      animateScale: false
+      animateScale: true
     }
   }
 });
