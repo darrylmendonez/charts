28486c3a4345f1c237677585f26238bf63d0c871
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2019-04-05 09:21:51 -0400

    added radar chart

---
+++ js/radar-graph.js
@@ -0,0 +1,26 @@
+const RADARCHART = document.getElementById("radar-chart");
+
+Chart.defaults.scale.ticks.beginAtZero = true;
+
+let radarChart = new Chart(RADARCHART, {
+  type: 'radar',
+  data: {
+    labels: ['Strength', 'Skill', 'Health', 'Speed', 'Luck'],
+    datasets: [
+      {
+        label: 'Points',
+        backgroundColor: 'rgba(00, 255, 00, 0.1)',
+        borderColor: '#00FF00',
+        borderWidth: 2,
+        data: [10, 20, 55, 30, 10],
+      },
+      {
+        label: 'Points',
+        backgroundColor: 'rgba(0, 255, 255, 0.1)',
+        borderColor: '#00FFFF',
+        borderWidth: 2,
+        data: [5, 100, 20, 70, 80],
+      }
+    ]
+  }
+});
